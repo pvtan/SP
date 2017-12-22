382c32fa3782f71299b7566612cb9391b3ce2536
pvtan <pvtan@up.edu.ph>
2017-12-22 22:12:20 +0800

    Added config file and improved spell checker usage

--- server.R
+++ server.R
@@ -6,7 +6,7 @@
 library(shiny)
 library(base64enc)
 library(twitteR)
-library(hunspell)
+source("config.R")
 
 shinyServer(
   function(input, output) {
@@ -15,15 +15,13 @@ shinyServer(
       bad_words <- hunspell_find(movie)
       sugg <- hunspell_suggest(bad_words[[1]])
       print(bad_words[[1]])
-      sugg <- unlist(sugg[[1]])
+      sugg <- unlist(sugg)
       print(sugg)
-      movie <- gsub(bad_words[[1]], sugg[[1]], movie)
+      if (!is.null(sugg)) { 
+        movie <- gsub(bad_words[[1]], sugg[[1]], movie) 
+      }
       print(movie)
       
-      api_key <- "6vLKhDh7UJdO1vMwYOnDbrr91"
-      api_secret <- "6UY2wIWDTSrOgpQVziA9sUFlPBIDhodcUmmz2KheFLkXc0xJF8"
-      access_token <- "1328460499-EmARRTuftN6dqehvZ7CgeNKqIpB0JIWoT3EvjKi"
-      access_token_secret <- "1Sz7gKFQS8Gz0Y1zln7XO9gXIWccsw9hgrBeuoFhUdyrS"
       setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
       
       tweets <- searchTwitter(input$query, n=100, lang="en")
