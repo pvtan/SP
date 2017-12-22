daaf707751c4a2f6d3745221b0a5c538c07e13c7
pvtan <pvtan@up.edu.ph>
2017-12-22 23:34:32 +0800

    Progress bar for retrieving tweets

--- server.R
+++ server.R
@@ -2,15 +2,20 @@
 #install.packages("base64enc")
 #install.packages("twitteR")
 #install.packages("hunspell")
+#install.packages("tm")
+#install.packages("SnowballC")
 
 library(shiny)
 library(base64enc)
 library(twitteR)
+#library(tm)
+#library(SnowballC)
 source("config.R")
 
 shinyServer(
   function(input, output) {
     observeEvent(input$enter, {
+      #spell checker
       movie <- input$query
       bad_words <- hunspell_find(movie)
       sugg <- hunspell_suggest(bad_words[[1]])
@@ -22,11 +27,20 @@ shinyServer(
       }
       print(movie)
       
-      setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
+      #retrieve tweets
+      withProgress(message = 'Retrieving tweets', value = 0, {
+        setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
+        tweets <- searchTwitter(input$query, n=100, lang="en")
+        n <- 10 # Number of times we'll go through the loop
+        for (i in 1:n) {
+          incProgress(1/n) # Increment the progress bar, and update the detail text.
+          Sys.sleep(0.1) # Pause for 0.1 seconds to simulate a long computation.
+        }
+      })
       
-      tweets <- searchTwitter(input$query, n=100, lang="en")
       tweets.df <- twListToDF(tweets)
-      
+      tweet_vector <- unlist(tweets.df["text"], use.names=FALSE)
+      #print(tweet_vector)
       output$tweets <- renderDataTable(tweets.df["text"])
     })
   }
