3b0e72d028352591f1c501db1d980b9cf96f58bb
pvtan <pvtan@up.edu.ph>
2017-12-28 15:13:00 +0800

    Removed hashtags and completed stemmed words

--- server.R
+++ server.R
@@ -42,7 +42,6 @@ shinyServer(
       if (!is.null(sugg)) { 
         movie <- gsub(bad_words[[1]], sugg[[1]], movie) 
       }
-      print(movie)
       
       #retrieve tweets 
       #from https://shiny.rstudio.com/articles/progress.html
@@ -60,18 +59,17 @@ shinyServer(
       tweets.df$screenName <- tolower(tweets.df$screenName)
       movie <- input$query
       movie <- tolower(movie)
-      tweets.df <- tweets.df[- grep(movie, tweets.df$screenName),]
-      print(tweets.df)
+      tweets.df <- tweets.df[- grep(movie, tweets.df$screenName),] #movie name not found in screen name
       output$tweets <- renderDataTable(tweets.df["text"])
       tweets.df["text"] <- sapply(tweets.df["text"],
                           function(row) iconv(row, "latin1", "ASCII", sub="")) #to remove emojis
       tweet_vector <- unlist(tweets.df["text"], use.names=FALSE)
-      corpus <- (VectorSource(tweet_vector))
-      corpus <- Corpus(corpus)
+      corpus <- Corpus(VectorSource(tweet_vector))
       for (i in 1:length(corpus$content)) {
         #from https://stackoverflow.com/questions/31348453/how-do-i-clean-twitter-data-in-r
         corpus[[i]]$content = gsub("&amp", "", corpus[[i]]$content)
         corpus[[i]]$content = gsub("@\\w+", "", corpus[[i]]$content) #remove handles
+        corpus[[i]]$content = gsub("#\\w+", "", corpus[[i]]$content) #remove hashtags
         corpus[[i]]$content = gsub("http.+", "", corpus[[i]]$content) #remove links
         corpus[[i]]$content = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", corpus[[i]]$content)
         corpus[[i]]$content = gsub("RT", "", corpus[[i]]$content) #remove RT labels
@@ -81,19 +79,17 @@ shinyServer(
       corpus <- tm_map(corpus, content_transformer(removePunctuation)) #remove punctuations
       corpus <- tm_map(corpus, content_transformer(removeWords), 
                        stopwords("english")) #remove stop words
-      corpus_copy <- corpus
-      corpus <- tm_map(corpus, stemDocument)
-      #corpus <- tm_map(corpus, stemCompletion, dictionary=corpus_copy)
+      dict <- ""
+      for (i in 1:length(corpus$content)) dict <- paste(dict, corpus[[i]]$content, sep=" ")
+      dict <- strsplit(dict, "\\s+")
+      dict <- unlist(dict)
       for (i in 1:length(corpus$content)) {
-        correction <- PlainTextDocument(stripWhitespace
-                                        (paste
-                                          (stemCompletion
-                                            (unlist
-                                              (strsplit
-                                                (as.character(corpus[[i]]$content)," ")),
-                                                dictionary=corpus_copy, type="shortest"),
-                                                sep="", collapse=" ")))
-        #if(correction[[1]] != "NA") corpus[[i]]$content <- correction[[1]]
+        tw <- strsplit(corpus[[i]]$content, "\\s+")
+        tw <- unlist(tw)
+        tw <- stemCompletion(tw, dict)
+        tw <- paste(tw, collapse=' ')
+        print(tw)
+        corpus[[i]]$content <- tw
       }
       output$processed_tweets <- renderTable(corpus$content)
     })
