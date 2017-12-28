7d6242c904f9a70ab92a30dc363ba005d6acfa3b
pvtan <pvtan@up.edu.ph>
2017-12-28 12:57:29 +0800

    Removed config file

--- config.R
+++
@@ -1,4 +0,0 @@
-api_key <- "6vLKhDh7UJdO1vMwYOnDbrr91"
-api_secret <- "6UY2wIWDTSrOgpQVziA9sUFlPBIDhodcUmmz2KheFLkXc0xJF8"
-access_token <- "1328460499-EmARRTuftN6dqehvZ7CgeNKqIpB0JIWoT3EvjKi"
-access_token_secret <- "1Sz7gKFQS8Gz0Y1zln7XO9gXIWccsw9hgrBeuoFhUdyrS"
